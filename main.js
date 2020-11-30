f06e89df2f6a8bc42b63492ec655de39cc77fdc2
jesseyproctor <jss.proctor@gmail.com>
2020-11-30 11:03:27 -0500

    4th-run

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const url = 'http://localhost:3000/movies'
 
+//  funciton to get movies
+function getMovies () {
+    fetch (url) 
+    .then (res => res.json())
+    .then (data => console.log(data))
+    // add call to function render
+}  
 
-fetch (url) 
-.then (res => res.json())
-   
+function createMovie () {
+    fetch (url) {
+        method: POST
+    }
+}
+
+
+
+
 
 
 
