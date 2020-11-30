e7b5669b4d88541bbfcdd321441f369a6a83714a
Tracy Falba <tfalba@mac.com>
2020-11-30 11:31:20 -0500

    final morning round 6

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const url = 'http://localhost:3000/movies'
 
 const submitButton = document.querySelector('#submit-button')
-
+const form = document.querySelector('#movie-form')
+submitButton.addEventListener('submit', function (event) {
+  event.preventDefault()
+  createMovie()
+})
 //  funciton to get movies
 function getMovies () {
-    fetch (url) 
+  fetch (url) 
     .then (res => res.json())
     .then (data => console.log(data))
     // add call to function render
-}  
+}
 
 function createMovie () {
-    fetch (url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify( {
-           title: 'title',
-        //    put the innerText here
-           watched: 'watched'
-        })
+  const getInput = document.querySelector('input').value
+
+  fetch (url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify( {
+      title: getInput,
+      //    put the innerText here
+      watched: 'watched'
     })
+  })
 }
 
 
